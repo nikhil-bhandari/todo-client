ef2b6cc66a58f5dc56d39cc3d64be034fff4bbdd
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-10-03 10:20:40 +0530

    Updated port number

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
 
-var NODE_PORT = 3000;
+var NODE_PORT = process.env.PORT || 3000;
 
-app.use("/bower_components", express.static(__dirname + "/../bower_components"))
+app.use("/bower_components", express.static(__dirname + "/../bower_components"));
 app.use(express.static(__dirname + "/../client/"));
 
 app.listen(NODE_PORT, function(){
